d3b78047fbe26d10c0cfec68f74e061b37ecde44
hudaiapp <109874271+hudaiapp@users.noreply.github.com>
2022-08-01 06:42:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/app/sw.js')
+    .register('/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
